7390bec50d1466caf01e2c73d36875b5dc8223ec
larencloete <56066363+larencloete@users.noreply.github.com>
2019-12-04 14:28:48 +0200

    My initial commit

---
+++ WeatherForMe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherLibrary;
+
+namespace WeatherForMe
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string weatherString = await GetWeather.GetTempAsync();
+            Console.WriteLine(weatherString);
+
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WeatherLibrary/GetWeather.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLibrary
+{
+    public class GetWeather
+    {
+        public static async Task<string> GetTempAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            String kit = await httpClient.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Cape%20Town&appid=60106437d06f8b03762ea496f3bd3643");
+
+            TheWeather theweather  = JsonConvert.DeserializeObject<TheWeather>(kit);
+            string returnString = string.Empty;
+
+            for (int index = 0; index < theweather.weather.Count; index++)
+            {
+                returnString = theweather.weather[index].description;
+
+            }
+
+       
+            return returnString;
+
+
+        }
+       
+
+
+
+    }
+}
